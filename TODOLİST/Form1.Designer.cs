7a4120742b25955f1046dafc41e5bead4b98a89b
nazlisayar <verassyr81@gmail.com>
2024-12-07 20:58:29 +0300

    Proje dosyası ekle.

---
+++ TODOLİST/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TODOLİST
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtgorev = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ckdlstgorev = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // txtgorev
+            // 
+            this.txtgorev.Location = new System.Drawing.Point(24, 33);
+            this.txtgorev.Name = "txtgorev";
+            this.txtgorev.Size = new System.Drawing.Size(100, 22);
+            this.txtgorev.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ckdlstgorev
+            // 
+            this.ckdlstgorev.FormattingEnabled = true;
+            this.ckdlstgorev.Location = new System.Drawing.Point(172, 12);
+            this.ckdlstgorev.Name = "ckdlstgorev";
+            this.ckdlstgorev.Size = new System.Drawing.Size(221, 395);
+            this.ckdlstgorev.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ckdlstgorev);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtgorev);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtgorev;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox ckdlstgorev;
+    }
+}
+
---
+++ TODOLİST/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODOLİST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string gorev=txtgorev.Text;
+            if (!string.IsNullOrEmpty(gorev))
+            {
+                ckdlstgorev.Items.Add(gorev);
+                txtgorev.Clear();
+            }
+            else
+            {
+                MessageBox.Show("lütfen bir görev girin","uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ckdlstgorev.SelectedItem != null)
+            {
+                ckdlstgorev.Items.Remove(ckdlstgorev.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("lütfen silmek istediğiniz öğeyi seçiniz", "uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+    }
+}
